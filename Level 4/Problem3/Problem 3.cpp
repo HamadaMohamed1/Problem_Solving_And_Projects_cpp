d077eb436f1553fd7b3127912fd5b6e1ffb8afab
HamadaMohamed1 <hamadamohamed1231255@gmail.com>
2024-07-29 15:16:11 +0300

    add problems 1 : 5

---
+++ Level 4/Problem3/Problem 3.cpp
@@ -0,0 +1,108 @@
+/*
+* Problem #3
+* Write a program to check if the year is a leap year or  not
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadNumber(string message)
+{
+    int num;
+    cout << message << endl;
+    cin >> num;
+    return num;
+}
+
+string NumberToText(int number)
+{
+    if (number == 0)
+    {
+        return "";
+    }
+
+    if (number >=1 && number <= 19)
+    {
+        string arr[] = {"","One ","Two ","Three ","Four ","Five ","Six ","Seven ",
+        "Eight ","Nine ","Ten ","Eleven ","Twelve ","Thirteen ","Fourteen ",
+        "Fifteen ","Sixteen ","Seventeen ","Eighteen ","Ninteen "};
+        return arr[number];
+    }
+
+    if (number >= 20 && number <= 99)
+    {
+        string arr[] = {"","","Twenty ","Thirty ","Forty ","Fifty ","Sixty ",
+        "Seventy ","Eighty ","Nintey "};
+        return arr[number / 10] + NumberToText(number % 10);
+    }
+
+    if (number >= 100 && number <= 199)
+    {
+        return "One Handred " + NumberToText(number % 100);
+    }
+
+    if (number >= 200 && number <= 999)
+    {
+        return NumberToText(number / 100) + "Handreds " + NumberToText(number % 100);
+    }
+
+    if (number >= 1000 && number <= 1999)
+    {
+        return "One Thounsand " + NumberToText(number % 1000);
+    }
+
+    if (number >= 2000 && number <= 999999)
+    {
+        return NumberToText(number / 1000) + "Thounsands " + NumberToText(number % 1000);
+    }
+
+    if (number >= 1000000 && number <= 1999999)
+    {
+        return "One Million " + NumberToText(number % 1000000);
+    }
+
+    if (number >= 2000000 && number <= 999999999)
+    {
+        return NumberToText(number / 1000000) + "Millions " + NumberToText(number % 1000000);
+    }
+
+    if (number >= 1000000000 && number <= 1999999999)
+    {
+        return"One Billion " + NumberToText(number % 1000000000);
+    } 
+    else    
+    { 
+        return   NumberToText(number / 1000000000) + "Billions " + NumberToText(number % 1000000000);
+    }
+}
+
+bool IsLeapYear(int year)
+{
+    //if (year % 400 == 0)/*leap year if divisable by 400*/
+    //{
+    //    return true;
+    //}
+    //else if (year % 4 == 0 && year % 100 != 0)/*leap year if divisable by 4 and not ending with 00 i.e 1900*/
+    //{
+    //    return true;
+    //}
+    //else
+    //{
+    //    return false;
+    //}
+    
+   // return (year % 400 == 0) ? true : (year % 4 == 0 && year % 100 != 0) ? true : false;
+    return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0));
+}
+int main()
+{
+    
+    int year = ReadNumber("Enter the year");
+    cout << IsLeapYear(year);
+
+
+
+
+    return 0;
+}
+
